3d6d0ac7e7a38915dde24ba6fbf12d32d96cf92d
wiegine@ech.dev <wiegine@ech.dev>
2024-06-25 03:03:16 +0800

    ulit

--- _worker.js
+++ _worker.js
@@ -5,10 +5,24 @@
 // source: https://github.com/zizifn/edgetunnel
 
 import { connect } from 'cloudflare:sockets';
-import { uuid } from 'uuidv4';
-const userID = uuid();
-const gen = () => {
-  return uuid()
+function gg() {
+  var d = new Date().getTime();
+  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16;
+    if (d > 0) {
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+
+  let userID = gg();
+  const gen = () => {
+  return gg();
 };
 let proxyIP = '';
 if (!isValidUUID(userID)) {
